e6a3a7fbf64c4503c895f4a34f1d69afc9a3d838
thiagonj <thiago.n.j@hotmail.com>
2025-04-08 14:38:45 -0300

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 import torch.nn.functional as F
 
+# Carrega o modelo
 @st.cache_resource
 def load_model():
     model_name = "pierreguillou/bert-base-cased-sentiment-analysis-sst-2"
@@ -10,23 +11,22 @@ def load_model():
     model = AutoModelForSequenceClassification.from_pretrained(model_name)
     return tokenizer, model
 
-def interpretar_logits(logits):
-    probabilities = F.softmax(logits, dim=-1)[0].tolist()
-    sentimentos = ["negativo", "positivo"]  # Ordem usada no modelo SST-2
-    predicted_index = probabilities.index(max(probabilities))
-    return sentimentos[predicted_index], probabilities[predicted_index]
-
+# Interface
 st.title("Análise de Sentimentos - Projeto Ascend")
 texto = st.text_area("Digite um comentário de cliente:")
+
+def interpretar_logits(logits):
+    probabilities = F.softmax(logits, dim=-1)[0].tolist()
+    sentimentos = ["negativo", "positivo"]  # Esse modelo só tem essas duas classes
+    predicted_index = probabilities.index(max(probabilities))
+    return sentimentos[predicted_index], probabilities[predicted_index]
 
 if texto:
     tokenizer, model = load_model()
     inputs = tokenizer(texto, return_tensors="pt", truncation=True, padding=True, max_length=512)
-
     with torch.no_grad():
         outputs = model(**inputs)
     logits = outputs.logits
-
     sentimento, confianca = interpretar_logits(logits)
     confianca_percent = round(confianca * 100, 2)
 
