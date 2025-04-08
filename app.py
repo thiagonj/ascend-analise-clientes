5182442b77492bfef3eb3ef4ee954c6558707ac1
thiagonj <thiago.n.j@hotmail.com>
2025-04-08 14:36:21 -0300

    app.py

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ import torch.nn.functional as F
 
 @st.cache_resource
 def load_model():
-    model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
+    model_name = "pierreguillou/bert-base-cased-sentiment-analysis-sst-2"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForSequenceClassification.from_pretrained(model_name)
     return tokenizer, model
 
 def interpretar_logits(logits):
     probabilities = F.softmax(logits, dim=-1)[0].tolist()
-    estrelas = ["1 estrela", "2 estrelas", "3 estrelas", "4 estrelas", "5 estrelas"]
+    sentimentos = ["negativo", "positivo"]  # Ordem usada no modelo SST-2
     predicted_index = probabilities.index(max(probabilities))
-    return estrelas[predicted_index], probabilities[predicted_index]
+    return sentimentos[predicted_index], probabilities[predicted_index]
 
 st.title("Análise de Sentimentos - Projeto Ascend")
 texto = st.text_area("Digite um comentário de cliente:")
@@ -22,6 +22,7 @@ texto = st.text_area("Digite um comentário de cliente:")
 if texto:
     tokenizer, model = load_model()
     inputs = tokenizer(texto, return_tensors="pt", truncation=True, padding=True, max_length=512)
+
     with torch.no_grad():
         outputs = model(**inputs)
     logits = outputs.logits
